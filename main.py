e1065c396f67130872731720a84c8b435f221bdc
BlogPlayCode <99440944+BlogPlayCode@users.noreply.github.com>
2023-03-14 01:34:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import os
-import logging
-import marshal
+# импорты
+import os       # для работы с файловой системой
+import marshal  # для записи заметок в файл и их чтения
+import dotenv   # для безопасной подгрузки токена бота
+from aiogram import Bot, Dispatcher, executor, types  # для работы с API Telegram
 
-from aiogram import Bot, Dispatcher, executor, types
+# инициализация модулей и объявление  переменных
+dotenv.load_dotenv()            # подгрузка окружения
+API_TOKEN = os.getenv("token")  # получение токена
+bot = Bot(token=API_TOKEN)      # получение объекта бота
+dp = Dispatcher(bot)            # получение диспетчера бота
+launch_on_message = {}          # дополнительная переменная для ожидания ввода названия или текта
+temp = {}                       # дополнительная переменная для временных данных пользователя
 
-API_TOKEN = os.environ.get("token")
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-launch_on_message = {}
-temp = {}
-
-menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+# создание клавиатур с кнопками
+menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # клавиатура главного меню
 menu_markup.add(types.KeyboardButton("➕ Новая заметка"))
 menu_markup.add(types.KeyboardButton("👁‍ Мои заметки"))
 menu_markup.add(types.KeyboardButton("🧑‍🔧 Профиль"))
 
-settings_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+settings_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)  # клавиатура настроек
 settings_markup.row(types.KeyboardButton("🔄 Изменить порядок заметок"))
 settings_markup.add(types.KeyboardButton("🗑 Удалить данные"))
 settings_markup.add(types.KeyboardButton("✨ Mеню"))
 
 
+# вывод текста о включении бота
+async def on_ready(_):
+    print(f"{bot.me.full_name} is online")
+
+
+# удобное получение заметки
 def get_note(_id: int, filename: str):
     try:
         with open(f"Users/{_id}/{filename}", "rb") as f:
@@ -29,15 +38,8 @@ def get_note(_id: int, filename: str):
         return None
 
 
-@dp.message_handler(text="🗑 Удалить данные")
-async def delete_data(message: types.Message):
-    markup = types.InlineKeyboardMarkup()
-    markup.insert(types.InlineKeyboardButton("Да", callback_data="DeleteUserData"))
-    markup.insert(types.InlineKeyboardButton("Нет", callback_data="OpenMenu"))
-    await message.answer("Вы точно хотите удалить все данные? Это нельзя будет отменить", reply_markup=markup)
-
-
-@dp.message_handler(commands=['start'])
+# обработка начальной команды
+@dp.message_handler(commands=['start'])  # отслеживание команды start
 async def on_start(message: types.Message):
     await message.answer(
         "Приветствую, я бот для создания и сохранения заметок.")
@@ -47,24 +49,27 @@ async def on_start(message: types.Message):
     await on_message(message)
 
 
-async def add_new_note3(message: types.Message):
+# команды, вызываемые кнопками
+async def add_new_note3(message: types.Message):  # создание новой заметки(конец)
     if not message.text:
         return
-    with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "rb") as f:
-        note = marshal.loads(f.read())
-    note["content"] = message.text
-    with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "wb") as f:
-        f.write(marshal.dumps(note))
+    if message.text != "👌 Как сейчас":
+        with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "rb") as f:
+            note = marshal.loads(f.read())
+        note["content"] = message.html_text
+        with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "wb") as f:
+            f.write(marshal.dumps(note))
     await message.answer("✨ Заметка сохранена")
     global launch_on_message
     del launch_on_message[message.from_user.id]
     if f"{message.from_user.id}_{temp[message.from_user.id]}" in temp:
         await temp[f"{message.from_user.id}_{temp[message.from_user.id]}"].edit_text("Заметка была отредактирована")
         del temp[f"{message.from_user.id}_{temp[message.from_user.id]}"]
+    del temp[message.from_user.id]
     await on_message(message)
 
 
-async def add_new_note2(message: types.Message):
+async def add_new_note2(message: types.Message):  # создание новой заметки(ожидание текста)
     if not message.text:
         return
     if len(message.text) > 30:
@@ -73,27 +78,37 @@ async def add_new_note2(message: types.Message):
         )
     if message.from_user.id not in temp:
         temp[message.from_user.id] = message.message_id
+    try:
+        with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "rb") as f:
+            note = marshal.loads(f.read())
+    except:
+        note = {"title": "Untitled", "content": "None"}
+    note["title"] = message.text
     with open(f"Users/{message.from_user.id}/{temp[message.from_user.id]}.data", "wb") as f:
-        f.write(marshal.dumps({"title": message.text}))
-    await message.answer("Введите текст заметки")
+        f.write(marshal.dumps(note))
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(types.KeyboardButton("👌 Как сейчас"))
+    await message.answer("Введите текст заметки", reply_markup=markup)
     global launch_on_message
     launch_on_message[message.from_user.id] = add_new_note3
 
 
 @dp.message_handler(text=["➕ Новая заметка"])
-async def add_new_note(message: types.Message, user_id=None):
+async def add_new_note(message: types.Message, user_id=None):  # создание новой заметки(ожидание названия)
     if not user_id:
         user_id = message.from_user.id
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(types.KeyboardButton("👌 Как сейчас"))
     await message.answer(
         "Придумайте название для заметки, старайтесь сделать его коротким и понятным",
-        reply_markup=types.ReplyKeyboardRemove()
+        reply_markup=markup
     )
     global launch_on_message
     launch_on_message[user_id] = add_new_note2
 
 
 @dp.message_handler(text=["👁‍ Мои заметки"])
-async def view_notes(message: types.Message):
+async def view_notes(message: types.Message):  # показ заметок
     markup = types.InlineKeyboardMarkup(row_width=2)
     path = f"Users/{message.from_user.id}"
     if os.name == "nt":
@@ -138,7 +153,7 @@ async def view_notes(message: types.Message):
 
 
 @dp.message_handler(text=["🧑‍🔧 Профиль"])
-async def settings(message: types.Message):
+async def settings(message: types.Message):  # открытие настроек
     user = message.from_user
     mention = f"({user.mention})" if user.mention else ""
     await message.answer(f"""
@@ -149,8 +164,16 @@ ID: {user.id}
 """, reply_markup=settings_markup)
 
 
+@dp.message_handler(text="🗑 Удалить данные")  # переспрашиваем об удалении всех заметок
+async def delete_data(message: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    markup.insert(types.InlineKeyboardButton("Да", callback_data="DeleteUserData"))
+    markup.insert(types.InlineKeyboardButton("Нет", callback_data="OpenMenu"))
+    await message.answer("Вы точно хотите удалить все данные? Это нельзя будет отменить", reply_markup=markup)
+
+
 @dp.message_handler(text=["🔄 Изменить порядок заметок"])
-async def reverse(message: types.Message):
+async def reverse(message: types.Message):  # изменение порядка заметок
     if "reverse" in os.listdir(f"Users/{message.from_user.id}"):
         os.remove(f"Users/{message.from_user.id}/reverse")
     else:
@@ -159,19 +182,20 @@ async def reverse(message: types.Message):
     await message.answer("✅ Успешно")
 
 @dp.message_handler()
-async def on_message(message: types.Message):
+async def on_message(message: types.Message):  # во всех остальных случаях
     if message.from_user.id in launch_on_message:
         return await launch_on_message[message.from_user.id](message)
     await message.answer("✨ Главное меню", reply_markup=menu_markup)
 
 
+# отслеживание нажатий Inline кнопок
 @dp.callback_query_handler()
 async def callback_handler(callback: types.CallbackQuery):
-    if callback.data == "OpenMenu":
+    if callback.data == "OpenMenu":  # при нажатии "В меню"
         await callback.message.answer("✨ Главное меню", reply_markup=menu_markup)
-    elif callback.data == "DeleteThis":
+    elif callback.data == "DeleteThis":  # при нажатии на кнопку удалить сообщение
         await callback.message.delete()
-    elif callback.data == "DeleteUserData":
+    elif callback.data == "DeleteUserData":  # удаление заметок пользователя
         for filename in os.listdir(f"Users/{callback.from_user.id}"):
             try:
                 path = f"Users/{callback.from_user.id}/{filename}"
@@ -180,7 +204,7 @@ async def callback_handler(callback: types.CallbackQuery):
                 pass
         await callback.message.answer("✨ Главное меню", reply_markup=menu_markup)
         return await callback.answer("Все данные о вас удалены", show_alert=True)
-    elif callback.data.startswith("NextPage"):
+    elif callback.data.startswith("NextPage"):  # листание вперед в списке заметок
         num = float(int(callback.data[9:]) / 2)
         num2 = float(num + 3)
         notes = os.listdir(f"Users/{callback.from_user.id}")
@@ -213,7 +237,7 @@ async def callback_handler(callback: types.CallbackQuery):
             row.append(button_next)
         markup.row(*row)
         await callback.message.edit_reply_markup(markup)
-    elif callback.data.startswith("PrevPage"):
+    elif callback.data.startswith("PrevPage"):  # листание назад в списке заметок
         num = float(int(int(callback.data[9:])) / 2)
         num2 = float(num + 3)
         notes = os.listdir(f"Users/{callback.from_user.id}")
@@ -248,7 +272,7 @@ async def callback_handler(callback: types.CallbackQuery):
             row.append(button_next)
         markup.row(*row)
         await callback.message.edit_reply_markup(markup)
-    elif callback.data.startswith("ViewNote"):
+    elif callback.data.startswith("ViewNote"):  # отобразить выбранную заметку
         _id = callback.from_user.id
         filename = callback.data[9:]
         note = get_note(_id, filename)
@@ -256,24 +280,24 @@ async def callback_handler(callback: types.CallbackQuery):
         markup.insert(types.InlineKeyboardButton("Изменить заметку", callback_data=f"EditNote_{filename}"))
         markup.insert(types.InlineKeyboardButton("Удалить заметку", callback_data=f"RequestDelete_{filename}"))
         await callback.message.edit_text(
-            f"<strong>{note['title']}</strong>\n{note['content']}",
-            parse_mode=types.ParseMode.HTML
+            f"<strong>{note['title']}</strong>\n\n{note['content']}",
+            parse_mode=types.ParseMode.HTML,
+            reply_markup=markup
         )
-        await callback.message.edit_reply_markup(markup)
-    elif callback.data.startswith("EditNote"):
+    elif callback.data.startswith("EditNote"):  # изменить заметку
         filename = callback.data[9:-5]
         global temp
         temp[f"{callback.from_user.id}_{filename}"] = callback.message
         temp[callback.from_user.id] = filename
         await add_new_note(callback.message, callback.from_user.id)
-    elif callback.data.startswith("RequestDelete"):
+    elif callback.data.startswith("RequestDelete"):  # спросить о удалении заметки
         filename = callback.data[14:]
         markup = types.InlineKeyboardMarkup()
         markup.insert(types.InlineKeyboardButton("Да", callback_data=f"DeleteNote_{filename}"))
         markup.insert(types.InlineKeyboardButton("Нет", callback_data="DeleteThis"))
         await callback.message.answer("Вы уверены что хотите удалить эту заметку?", reply_markup=markup)
         temp[-1*callback.from_user.id] = callback.message.message_id
-    elif callback.data.startswith("DeleteNote"):
+    elif callback.data.startswith("DeleteNote"):  # удалить заметку
         filename = callback.data[11:]
         path = f"Users/{callback.from_user.id}/{filename}"
         os.remove(path)
@@ -286,6 +310,6 @@ async def callback_handler(callback: types.CallbackQuery):
     await callback.answer()
 
 
+# запуск программы
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_ready)
